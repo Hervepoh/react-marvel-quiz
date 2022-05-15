1ac4f7f133804855ba3b635812d95992334b1ee6
Hervepoh <epohherve63@gmail.com>
2022-05-15 18:08:31 +0100

    Landing Page Btn on landing with wolverine animation

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="footer-container">
+        <p>Projet réalisé par  @Rv merci à DonkeyGeek et à Grafikart </p>
+        <p>Les icones wolverine , Iron-man , spider-man, batman sont prise sur <a href="http://iconFinder.com" rel="noreferrer"  target="_blank">iconFinder.com</a> </p>
+      </div>
+    </footer>
   )
 }
 
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React , {useEffect,useRef,useState,Fragment} from 'react'
 
 const Landing = () => {
+
+  const [btn, setbtn] = useState(false);
+
+  const refWolverine = useRef(null);
+  useEffect(() => {
+    refWolverine.current.classList.add('startingImg');
+    setTimeout(() =>{
+      refWolverine.current.classList.remove('startingImg');
+      setbtn(true);
+    },1000);
+  }, [])
+
+  const displayBtn = btn && (<><div className="leftBox">
+                                  <button className="btn btn-welcome">Inscription</button>
+                                </div>
+                                <div className="rightBox">
+                                  <button className="btn btn-welcome">Inscription</button>
+                                </div>
+                              </>);
+
   return (
-    <div>Landing</div>
+    <main className="welcomePage" ref={ refWolverine }>
+    { displayBtn }
+    </main>
   )
 }
 
