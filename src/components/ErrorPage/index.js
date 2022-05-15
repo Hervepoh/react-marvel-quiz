4e40c6b8bf5a9e70627be45b02ddc646669d7ddd
Hervepoh <epohherve63@gmail.com>
2022-05-16 00:06:29 +0100

    Error Page

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import img from '../../images/batman.png'
+import styled from 'styled-components';
 
 const ErrorPage = () => {
+  
   return (
     <div className="quiz-bg">
-      <div className="container">
-        404 ERROR
+      <div className="container"> 
+        <ErrorHeader>Oops, cette page n'existe pas</ErrorHeader>
+        <ErrorImg src={ img } alt="404 ERROR" />
+        <ErrorTag>Erreur 404</ErrorTag>
       </div> 
     </div>
   )
 }
+
+const ErrorTag= styled.p`
+  text-align : center;
+  font-size: 2rem
+`
+const ErrorHeader = styled.h2`
+  text-align : center;
+`
+const ErrorImg = styled.img`
+  display : block;
+  margin: 40px auto;
+`
 
 export default ErrorPage
