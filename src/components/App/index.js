fd987d1a2df1545bc54f3576401f8a80b4008ccd
Hervepoh <epohherve63@gmail.com>
2022-05-15 14:28:46 +0100

    Code component restruction of App.js , Add (Header,Footer and Landing) Component

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import '../../App.css';
+import Header from '../Header';
+import Footer from '../Footer';
+import Landing from '../Landing';
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Landing />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
